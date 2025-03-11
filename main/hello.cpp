a6661fcb496e2f5a50657334b05e81264cf09e31
yamanuri <yamanurinad18@gmail.com>
2025-03-11 15:51:10 +0530

    Changed

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << " i am studying in PES college";
   return 0;
 }
